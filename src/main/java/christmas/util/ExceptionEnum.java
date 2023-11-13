20e677cff5ef9b535a43f4d9f4f45361482aac40
linirini <2001yerin@naver.com>
2023-11-14 02:11:20 +0900

    feat : implement error message for invalid date input

--- src/main/java/christmas/util/ExceptionEnum.java
+++ src/main/java/christmas/util/ExceptionEnum.java
@@ -1,5 +1,18 @@
 package christmas.util;
 
-public enum
-ExceptionEnum {
+public enum ExceptionEnum {
+
+    INVALID_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+    private static final String NOTICE = "[ERROR] ";
+
+    private String message;
+
+    ExceptionEnum(String message) {
+        this.message = NOTICE + message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
 }
