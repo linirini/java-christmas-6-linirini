121e75e8dd478813ac24471b1d0c6df0a430cf66
linirini <2001yerin@naver.com>
2023-11-14 14:09:40 +0900

    refactor : change constructor argument Hashmap's key into MenuOption enum

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -10,16 +10,16 @@ public class Orders {
     private final HashMap<MenuOption,Integer> orders;
 
 
-    public Orders(HashMap<String, Integer> orders) {
+    public Orders(HashMap<MenuOption, Integer> orders) {
         validate(orders);
-        this.orders = convertToOrders(orders);
+        this.orders = orders;
     }
 
-    private void validate(HashMap<String, Integer> orders) {
+    private void validate(HashMap<MenuOption, Integer> orders) {
         throwIfMaximumOrderCountExceeded(orders);
     }
 
-    private void throwIfMaximumOrderCountExceeded(HashMap<String, Integer> menusAndCount) {
+    private void throwIfMaximumOrderCountExceeded(HashMap<MenuOption, Integer> menusAndCount) {
         int totalCount = 0;
         for (Integer count : menusAndCount.values()) {
             totalCount+=count;
@@ -27,10 +27,6 @@ public class Orders {
         if(totalCount>20){
             throw new IllegalArgumentException(MAXIMUM_ORDER_COUNT_EXCEEDED.getMessage());
         }
-    }
-
-    private HashMap<MenuOption, Integer> convertToOrders(HashMap<String, Integer> orders) {
-        return null;
     }
 
 }
