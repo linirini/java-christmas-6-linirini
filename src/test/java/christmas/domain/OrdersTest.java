fa0784e0d4d04f659112d4a5e6f02b757457c521
linirini <2001yerin@naver.com>
2023-11-14 20:22:18 +0900

    refactor : seperate method for creating orders with 2 menu

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -19,39 +19,38 @@ public class OrdersTest {
     @DisplayName("주문 개수가 20개 이상이면 예외를 던진다.")
     @Test
     void 주문_개수_초과() {
-        HashMap<MenuOption, Integer> orders = new HashMap<>();
-        orders.put(MUSHROOM_SOUP, 15);
-        orders.put(TAPAS, 15);
+        HashMap<MenuOption, Integer> orders = createOrders(MUSHROOM_SOUP, 15,TAPAS, 15);
         assertThatThrownBy(() -> new Orders(orders)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문 개수가 20개 이하이다.")
     @Test
     void 주문_개수_범위_내() {
-        HashMap<MenuOption, Integer> orders = new HashMap<>();
-        orders.put(MUSHROOM_SOUP, 5);
-        orders.put(TAPAS, 15);
+        HashMap<MenuOption, Integer> orders = createOrders(MUSHROOM_SOUP, 5,TAPAS, 15);
         assertThatNoException().isThrownBy(() -> new Orders(orders));
     }
 
     @DisplayName("음료만 주문할 경우 예외를 던진다.")
     @Test
     void 음료만_주문() {
-        HashMap<MenuOption, Integer> orders = new HashMap<>();
-        orders.put(ZERO_COKE, 5);
-        orders.put(CHAMPAGNE, 15);
+        HashMap<MenuOption, Integer> orders = createOrders(ZERO_COKE, 5,CHAMPAGNE, 15);
         assertThatThrownBy(() -> new Orders(orders)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문 메뉴명과 개수를 반환한다.")
     @Test
     void 주문_메뉴명과_개수_반환() {
-        HashMap<MenuOption, Integer> orders = new HashMap<>();
-        orders.put(ZERO_COKE, 5);
-        orders.put(TAPAS, 15);
+        HashMap<MenuOption, Integer> orders = createOrders(ZERO_COKE, 5,TAPAS, 15);
         Orders result = new Orders(orders);
         assertThat(result.getOrderedMenusAndCount()).hasSize(2)
                 .contains(entry("제로콜라", 5), entry("타파스", 15));
+    }
+
+    private HashMap<MenuOption, Integer> createOrders(MenuOption option1, int quantity1, MenuOption option2, int quantity2) {
+        HashMap<MenuOption, Integer> orders = new HashMap<>();
+        orders.put(option1, quantity1);
+        orders.put(option2, quantity2);
+        return orders;
     }
 
 }
