fdd8fe09467ca33f56a62a43afdb066c29dd68ef
linirini <2001yerin@naver.com>
2023-11-15 01:19:35 +0900

    test : test checking before christmas

--- src/test/java/christmas/util/DecemberCalenderTest.java
+++ src/test/java/christmas/util/DecemberCalenderTest.java
@@ -1,25 +1,35 @@
 package christmas.util;
 
+import static christmas.util.DecemberCalender.isAfterChristmas;
 import static christmas.util.DecemberCalender.isValidDate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DecemberCalenderTest {
 
     @DisplayName("이벤트 기간에 포함된다.")
-    @Test
-    void 이벤트_기간_포함() {
-        int date = 31;
+    @ParameterizedTest
+    @ValueSource(ints={23,31})
+    void 이벤트_기간_포함(int date) {
         assertThat(isValidDate(date)).isTrue();
     }
 
     @DisplayName("이벤트 기간에 포함되지 않는다.")
-    @Test
-    void 이벤트_기간_미포함() {
-        int date = 34;
+    @ParameterizedTest
+    @ValueSource(ints={32,34})
+    void 이벤트_기간_미포함(int date) {
         assertThat(isValidDate(date)).isFalse();
+    }
+
+    @DisplayName("크리스마스 전이다.")
+    @ParameterizedTest
+    @ValueSource(ints={1,3,25})
+    void 크리스마스_전(int date){
+        assertThat(isAfterChristmas(date)).isFalse();
     }
 
 }
