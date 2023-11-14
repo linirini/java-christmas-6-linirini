caf0b13677fc769ae384e38b2cea4239e0c23888
linirini <2001yerin@naver.com>
2023-11-15 03:15:28 +0900

    feat : implement special discount

--- src/main/java/christmas/domain/event/discount/SpecialDiscount.java
+++ src/main/java/christmas/domain/event/discount/SpecialDiscount.java
@@ -1,4 +1,16 @@
 package christmas.domain.event.discount;
 
+import static christmas.util.DecemberCalender.hasStar;
+
 public class SpecialDiscount {
+
+    private final static int DISCOUNT = 1000;
+
+    public int discount(int date){
+        if(hasStar(date)){
+            return DISCOUNT;
+        }
+        return 0;
+    }
+
 }
