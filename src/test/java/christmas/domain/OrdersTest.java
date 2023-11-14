4da685d273940c114e4681b313b2262912462b9b
linirini <2001yerin@naver.com>
2023-11-14 14:40:49 +0900

    test : test for ordering menu count in range

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import static christmas.domain.menu.MenuOption.MUSHROOM_SOUP;
 import static christmas.domain.menu.MenuOption.TAPAS;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.domain.menu.MenuOption;
@@ -18,6 +19,15 @@ public class OrdersTest {
         orders.put(MUSHROOM_SOUP, 15);
         orders.put(TAPAS, 15);
         assertThatThrownBy(() -> new Orders(orders)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 개수가 20개 이하이다..")
+    @Test
+    void 주문_개수_범위_내() {
+        HashMap<MenuOption, Integer> orders = new HashMap<>();
+        orders.put(MUSHROOM_SOUP, 5);
+        orders.put(TAPAS, 15);
+        assertThatNoException().isThrownBy(() -> new Orders(orders));
     }
 
 }
