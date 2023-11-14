6d786c92305fed5739f0e8e1e294c43dbbd47d77
linirini <2001yerin@naver.com>
2023-11-15 01:17:06 +0900

    test : test checking invalid date

--- src/test/java/christmas/util/DecemberCalenderTest.java
+++ src/test/java/christmas/util/DecemberCalenderTest.java
@@ -15,4 +15,11 @@ class DecemberCalenderTest {
         assertThat(isValidDate(date)).isTrue();
     }
 
+    @DisplayName("이벤트 기간에 포함되지 않는다.")
+    @Test
+    void 이벤트_기간_미포함() {
+        int date = 34;
+        assertThat(isValidDate(date)).isFalse();
+    }
+
 }
