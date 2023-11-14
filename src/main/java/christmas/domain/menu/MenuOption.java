52a8a02f3f7cd5a2e37e3aa1ef8756ec70e121e2
linirini <2001yerin@naver.com>
2023-11-14 14:26:22 +0900

    feat : implement finding menu option enum by view name

--- src/main/java/christmas/domain/menu/MenuOption.java
+++ src/main/java/christmas/domain/menu/MenuOption.java
@@ -1,19 +1,21 @@
 package christmas.domain.menu;
 
+import java.util.Arrays;
+
 public enum MenuOption {
 
-    MUSHROOM_SOUP("양송이수프",6000),
-    TAPAS("타파스",5500),
-    CAESAR_SALAD("시저샐러드",8000),
-    T_BONE_STEAK("티본스테이크",55000),
-    BBQ_RIBS("바비큐립",54000),
-    SEAFOOD_PASTA("해산물파스타",35000),
-    CHRISTMAS_PASTA("크리스마스파스타",25000),
-    CHOCO_CAKE("초코케이크",15000),
-    ICE_CREAM("아이스크림",5000),
-    ZERO_COKE("제로콜라",3000),
-    RED_WINE("레드와인",60000),
-    CHAMPAGNE("샴페인",25000);
+    MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CAESAR_SALAD("시저샐러드", 8000),
+    T_BONE_STEAK("티본스테이크", 55000),
+    BBQ_RIBS("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000),
+    CHOCO_CAKE("초코케이크", 15000),
+    ICE_CREAM("아이스크림", 5000),
+    ZERO_COKE("제로콜라", 3000),
+    RED_WINE("레드와인", 60000),
+    CHAMPAGNE("샴페인", 25000);
 
     private final String viewName;
 
@@ -24,12 +26,18 @@ public enum MenuOption {
         this.price = price;
     }
 
-    public String getViewName(){
+    public String getViewName() {
         return this.viewName;
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
+    }
+
+    public static MenuOption findMenuOption(String viewName) {
+        return Arrays.stream(MenuOption.values())
+                .filter(menuOption -> menuOption.viewName.equals(viewName)).findAny()
+                .orElseThrow();
     }
 
 }
