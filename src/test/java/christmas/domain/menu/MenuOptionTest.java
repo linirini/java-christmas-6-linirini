1d5e23c32919a76ba9585b2561ddaf526b8494d4
linirini <2001yerin@naver.com>
2023-11-14 14:43:05 +0900

    test : test for finding menu option enum by view name

--- src/test/java/christmas/domain/menu/MenuOptionTest.java
+++ src/test/java/christmas/domain/menu/MenuOptionTest.java
@@ -1,5 +1,7 @@
 package christmas.domain.menu;
 
+import static christmas.domain.menu.MenuOption.TAPAS;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +14,19 @@ public class MenuOptionTest {
     void 없는_메뉴() {
         assertThatThrownBy(() -> MenuOption.findMenuOption("없는 메뉴")).isInstanceOf(
                 RuntimeException.class);
+    }
+
+    @DisplayName("메뉴 이름에 맞는 enum을 반환한다.")
+    @Test
+    void 메뉴_찾기_성공() {
+        //given
+        String viewName = "타파스";
+
+        //when
+        MenuOption menuOption = MenuOption.findMenuOption(viewName);
+
+        //then
+        assertThat(menuOption).isEqualTo(TAPAS);
     }
 
 }
