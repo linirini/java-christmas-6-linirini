e7d2d61c4f9d66adb14597583aba47b02e01f35f
linirini <2001yerin@naver.com>
2023-11-14 13:53:48 +0900

    refactor : move throw if total count More than 20 exception to Orders domain

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,5 +1,7 @@
 package christmas.domain;
 
+import static christmas.util.ExceptionEnum.INVALID_ORDERS;
+
 import christmas.domain.menu.MenuOption;
 import java.util.HashMap;
 
@@ -9,7 +11,22 @@ public class Orders {
 
 
     public Orders(HashMap<String, Integer> orders) {
+        validate(orders);
         this.orders = convertToOrders(orders);
+    }
+
+    private void validate(HashMap<String, Integer> orders) {
+        throwIfInvalidTotalCount(orders);
+    }
+
+    private void throwIfInvalidTotalCount(HashMap<String, Integer> menusAndCount) {
+        int totalCount = 0;
+        for (Integer count : menusAndCount.values()) {
+            totalCount+=count;
+        }
+        if(totalCount>20){
+            throw new IllegalArgumentException();
+        }
     }
 
     private HashMap<MenuOption, Integer> convertToOrders(HashMap<String, Integer> orders) {
