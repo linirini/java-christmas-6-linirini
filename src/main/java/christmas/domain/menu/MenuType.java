9ad029f072bf683eed25d1a796d76ad77e66149c
linirini <2001yerin@naver.com>
2023-11-14 19:20:36 +0900

    feat : implement finding menu type by menu option view name

--- src/main/java/christmas/domain/menu/MenuType.java
+++ src/main/java/christmas/domain/menu/MenuType.java
@@ -1,4 +1,35 @@
 package christmas.domain.menu;
 
+import java.util.Arrays;
+
 public enum MenuType {
+
+    APPETIZER("에피타이저",
+            new MenuOption[]{MenuOption.MUSHROOM_SOUP, MenuOption.TAPAS, MenuOption.CAESAR_SALAD}),
+    MAIN_DISH("메인",
+            new MenuOption[]{MenuOption.T_BONE_STEAK, MenuOption.BBQ_RIBS, MenuOption.SEAFOOD_PASTA,
+                    MenuOption.CHRISTMAS_PASTA}),
+    DESSERT("디저트", new MenuOption[]{MenuOption.CHOCO_CAKE, MenuOption.ICE_CREAM}),
+    BEVERAGE("음료",
+            new MenuOption[]{MenuOption.ZERO_COKE, MenuOption.RED_WINE, MenuOption.CHAMPAGNE});
+
+    private final String viewName;
+
+    private final MenuOption[] containMenu;
+
+    MenuType(String viewName, MenuOption[] containMenu) {
+        this.viewName = viewName;
+        this.containMenu = containMenu;
+    }
+
+
+    public static MenuType findMenuType(String target) {
+        return Arrays.stream(MenuType.values()).filter(group -> hasMenuOption(group, target))
+                .findAny().get();
+    }
+
+    private static boolean hasMenuOption(MenuType group, String target) {
+        return Arrays.stream(group.containMenu).anyMatch(contain -> contain.getViewName().equals(target));
+    }
+
 }
