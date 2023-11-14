cf4d5b4df7e0b9876ad1eb51863ddf59517bfe51
linirini <2001yerin@naver.com>
2023-11-14 20:39:30 +0900

    feat :implement printing gift menu

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,10 @@
 package christmas.view;
 
+import static christmas.view.viewenum.OutputEnum.COUNT_UNIT;
+import static christmas.view.viewenum.OutputEnum.GIFT;
+import static christmas.view.viewenum.OutputEnum.GIFT_MENU_OUTPUT;
+import static christmas.view.viewenum.OutputEnum.MONEY_UNIT;
+import static christmas.view.viewenum.OutputEnum.NONE;
 import static christmas.view.viewenum.OutputEnum.ORDER_AMOUNT_OUTPUT;
 import static christmas.view.viewenum.OutputEnum.ORDER_MENU_OUTPUT;
 
@@ -8,19 +13,25 @@ import java.util.Map.Entry;
 
 public class OutputView {
 
-    private static final String COUNT_UNIT = "개";
-    private static final String MONEY_UNIT = "원";
-
     public void printOrderedMenus(HashMap<String, Integer> menus) {
         System.out.println(ORDER_MENU_OUTPUT.getMessage());
         for (Entry<String, Integer> entry : menus.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue() + COUNT_UNIT);
+            System.out.println(entry.getKey() + " " + entry.getValue() + COUNT_UNIT.getMessage());
         }
     }
 
     public void printOrdersAmount(int amount) {
         System.out.println(ORDER_AMOUNT_OUTPUT.getMessage());
-        System.out.println(String.format("%,d", amount) + MONEY_UNIT);
+        System.out.println(String.format("%,d", amount) + MONEY_UNIT.getMessage());
+    }
+
+    public void printGiftMenu(boolean receiveGift) {
+        System.out.println(GIFT_MENU_OUTPUT.getMessage());
+        if (receiveGift) {
+            System.out.println(GIFT.getMessage());
+            return;
+        }
+        System.out.println(NONE.getMessage());
     }
 
 }
--- src/main/java/christmas/view/viewenum/OutputEnum.java
+++ src/main/java/christmas/view/viewenum/OutputEnum.java
@@ -1,9 +1,16 @@
 package christmas.view.viewenum;
 
+import static christmas.domain.menu.MenuOption.CHAMPAGNE;
+
 public enum OutputEnum {
 
     ORDER_MENU_OUTPUT("<주문 메뉴>"),
-    ORDER_AMOUNT_OUTPUT("<할인 전 총주문 금액>");
+    COUNT_UNIT("개"),
+    ORDER_AMOUNT_OUTPUT("<할인 전 총주문 금액>"),
+    MONEY_UNIT("원"),
+    GIFT(CHAMPAGNE.getViewName()+" 1개"),
+    NONE("없음"),
+    GIFT_MENU_OUTPUT("<증정 메뉴>");
 
     private final String message;
 
