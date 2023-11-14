62885d057280c75eac63e4f427926c5576c7a9fa
linirini <2001yerin@naver.com>
2023-11-15 02:46:07 +0900

    feat : implement getting week/weekend discount

--- src/main/java/christmas/domain/event/discount/DayOfTheWeekDiscount.java
+++ src/main/java/christmas/domain/event/discount/DayOfTheWeekDiscount.java
@@ -1,4 +1,17 @@
 package christmas.domain.event.discount;
 
+import christmas.domain.Orders;
+
 public class DayOfTheWeekDiscount {
+
+    private static int DISCOUNT = 2023;
+
+    public int weekDiscount(Orders orders,int date){
+        return DISCOUNT*orders.getDessertCount();
+    }
+
+    public int weekendDiscount(Orders orders,int date){
+        return DISCOUNT*orders.getMainDishCount();
+    }
+
 }
