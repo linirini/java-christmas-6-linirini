a6cc697f0c0b54aab6a69abb4e9d5dcc3523ccd8
linirini <2001yerin@naver.com>
2023-11-14 23:52:45 +0900

    refactor: move determining qualification for gift to GiftEvent

--- src/main/java/christmas/domain/event/GiftEvent.java
+++ src/main/java/christmas/domain/event/GiftEvent.java
@@ -1,12 +1,23 @@
 package christmas.domain.event;
 
 import christmas.domain.menu.MenuOption;
+import java.awt.Menu;
+import java.util.HashMap;
 
 public class GiftEvent {
 
     private final static MenuOption GIFT = MenuOption.CHAMPAGNE;
+    private final static int COUNT = 1;
 
-    public static boolean canReceiveGift(int amount) {
+    public HashMap<String,Integer> getGift(int amount){
+        HashMap<String,Integer> gift = new HashMap<>();
+        if(canReceiveGift(amount)){
+            gift.put(GIFT.getViewName(),COUNT);
+        }
+        return gift;
+    }
+
+    private boolean canReceiveGift(int amount) {
         return amount >= 120000;
     }
 
--- src/test/java/christmas/domain/event/GiftEventTest.java
+++ src/test/java/christmas/domain/event/GiftEventTest.java
@@ -1,17 +1,26 @@
 package christmas.domain.event;
 
+import static java.util.Map.entry;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GiftEventTest {
 
+    private GiftEvent giftEvent;
+
+    @BeforeEach
+    void init() {
+        giftEvent = new GiftEvent();
+    }
+
     @DisplayName("할인 전 총주문 금액이 12만원 이상일 경우 증정품을 받을 수 있다.")
     @Test
     void 증정품_자격_여부() {
         int amount = 123000;
-        assertThat(GiftEvent.canReceiveGift(amount)).isTrue();
+        assertThat(giftEvent.getGift(amount)).hasSize(1).contains(entry("샴페인",1));
     }
 
 }
