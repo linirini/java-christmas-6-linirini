b03196754cecbd9db5f0aedb98766ef3d3760988
linirini <2001yerin@naver.com>
2023-11-15 17:40:15 +0900

    feat : implement badge enum

--- src/main/java/christmas/domain/event/badge/Badge.java
+++ src/main/java/christmas/domain/event/badge/Badge.java
@@ -1,4 +1,25 @@
 package christmas.domain.event.badge;
 
 public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000),
+    NO_BADGE("없음", 0);
+
+    private final String viewName;
+
+    private final int baseAmount;
+
+    Badge(String viewName, int baseAmount) {
+        this.viewName = viewName;
+        this.baseAmount = baseAmount;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public int getBaseAmount() {
+        return baseAmount;
+    }
 }
