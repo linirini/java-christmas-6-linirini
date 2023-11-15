e78464cf528c88d2e5d37be18439dc4759f1d568
linirini <2001yerin@naver.com>
2023-11-15 19:25:18 +0900

    test : test for calculating total Event Benefit

---
+++ src/test/java/christmas/domain/event/EventBenefitTest.java
@@ -0,0 +1,17 @@
+package christmas.domain.event;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventBenefitTest {
+
+    @DisplayName("총혜택 금액을 반환한다.")
+    @Test
+    void 총혜택_금액_계산() {
+        assertThat(new EventBenefit(1000, 0, 2023, 1000, 25000).getTotalEventBenefit()).isEqualTo(
+                29023);
+    }
+
+}
