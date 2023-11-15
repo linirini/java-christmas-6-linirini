32301abe92bfeec8be1f688c42e77a0b6eba87c0
linirini <2001yerin@naver.com>
2023-11-15 19:14:25 +0900

    fix : add checking if it is weekend

--- src/main/java/christmas/domain/event/discount/DayOfTheWeekDiscount.java
+++ src/main/java/christmas/domain/event/discount/DayOfTheWeekDiscount.java
@@ -1,17 +1,25 @@
 package christmas.domain.event.discount;
 
+import static christmas.util.DecemberCalender.isWeekend;
+
 import christmas.domain.Orders;
 
 public class DayOfTheWeekDiscount {
 
     private static int DISCOUNT = 2023;
 
-    public int weekDiscount(Orders orders,int date){
-        return DISCOUNT*orders.getDessertCount();
+    public int weekDiscount(int date, Orders orders){
+        if(!isWeekend(date)) {
+            return DISCOUNT * orders.getDessertCount();
+        }
+        return 0;
     }
 
-    public int weekendDiscount(Orders orders,int date){
-        return DISCOUNT*orders.getMainDishCount();
+    public int weekendDiscount(int date, Orders orders){
+        if(isWeekend(date)){
+            return DISCOUNT*orders.getMainDishCount();
+        }
+        return 0;
     }
 
 }
