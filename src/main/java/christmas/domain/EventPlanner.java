83170a895172400ef9c70228e1b5a01b0a1c5dc4
linirini <2001yerin@naver.com>
2023-11-15 19:26:29 +0900

    feat : implement event planner to get event benefit result

---
+++ src/main/java/christmas/domain/EventPlanner.java
@@ -0,0 +1,33 @@
+package christmas.domain;
+
+import christmas.domain.event.EventBenefit;
+import christmas.domain.event.Gift;
+import christmas.domain.event.badge.Badge;
+import christmas.domain.event.discount.ChristmasDdayDiscount;
+import christmas.domain.event.discount.DayOfTheWeekDiscount;
+import christmas.domain.event.discount.SpecialDiscount;
+
+public class EventPlanner {
+
+    private static final int MINIMUM_AMOUNT = 10000;
+
+    private ChristmasDdayDiscount christmasDdayDiscount = new ChristmasDdayDiscount();
+    private DayOfTheWeekDiscount dayOfTheWeekDiscount = new DayOfTheWeekDiscount();
+    private SpecialDiscount specialDiscount = new SpecialDiscount();
+
+    public boolean isMinimumAmountMetForEvent(int amount) {
+        return amount >= MINIMUM_AMOUNT;
+    }
+
+    public Gift getGift(int totalAmountBeforeDiscount) {
+        return Gift.receiveGift(totalAmountBeforeDiscount);
+    }
+
+    public EventBenefit getEventBenefit(int date, Orders orders,int totalAmountBeforeDiscount,Gift gift){
+        return new EventBenefit(christmasDdayDiscount.discount(date),
+                dayOfTheWeekDiscount.weekDiscount(date,orders),
+                dayOfTheWeekDiscount.weekendDiscount(date,orders), specialDiscount.discount(date),
+                gift.getGiftBenefit());
+    }
+
+}
