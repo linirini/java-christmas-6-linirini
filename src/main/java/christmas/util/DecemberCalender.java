d65bc14487dbed4809c8046ceb63bfe19c8367ce
linirini <2001yerin@naver.com>
2023-12-13 17:06:50 +0900

    feat : implement determining if the date is weekend

--- src/main/java/christmas/util/DecemberCalender.java
+++ src/main/java/christmas/util/DecemberCalender.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public enum DecemberCalender {
 
-    DECEMBER(31,List.of(3,10,17,24,25,31));
+    DECEMBER(31, List.of(3, 10, 17, 24, 25, 31));
 
     private static final int CHRISTMAS = 25;
     private static final int FIRST_WEEKEND_FRIDAY = 1;
@@ -17,23 +17,31 @@ public enum DecemberCalender {
         this.star = star;
     }
 
-    public static boolean isDdayEventTerm(int date){
-        return date<=CHRISTMAS;
+    public static boolean isDdayEventTerm(int date) {
+        return date <= CHRISTMAS;
     }
 
-    public static boolean isWeekend(int date){
+    public static boolean isWeekend(int date) {
+        return isFriday(date) || isSaturday(date);
+    }
+
+    private static boolean isFriday(int date) {
+        return (date - FIRST_WEEKEND_FRIDAY) % 7 == 0;
+    }
+
+    private static boolean isSaturday(int date) {
+        return (date - FIRST_WEEKEND_SATURDAY) % 7 == 0;
+    }
+
+    public static boolean isStarDate(int date) {
         return false;
     }
 
-    public static boolean isStarDate(int date){
+    public static boolean isEventTerm(int date) {
         return false;
     }
 
-    public static boolean isEventTerm(int date){
-        return false;
-    }
-
-    public static boolean isExistingDate(int date){
+    public static boolean isExistingDate(int date) {
         return false;
     }
 }
