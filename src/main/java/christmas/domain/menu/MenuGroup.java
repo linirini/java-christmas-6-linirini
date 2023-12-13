9e8c10d577a648bd3a21b84899b16d0d61b6e1ee
linirini <2001yerin@naver.com>
2023-12-13 17:33:58 +0900

    feat : create menu group enums

--- src/main/java/christmas/domain/menu/MenuGroup.java
+++ src/main/java/christmas/domain/menu/MenuGroup.java
@@ -1,4 +1,33 @@
 package christmas.domain.menu;
 
+import static christmas.domain.menu.MenuOption.BBQ_LIP;
+import static christmas.domain.menu.MenuOption.CAESAR_SALAD;
+import static christmas.domain.menu.MenuOption.CHAMPAGNE;
+import static christmas.domain.menu.MenuOption.CHOCO_CAKE;
+import static christmas.domain.menu.MenuOption.CHRISTMAS_PASTA;
+import static christmas.domain.menu.MenuOption.ICE_CREAM;
+import static christmas.domain.menu.MenuOption.MUSHROOM_SOUP;
+import static christmas.domain.menu.MenuOption.RED_WINE;
+import static christmas.domain.menu.MenuOption.SEAFOOD_PASTA;
+import static christmas.domain.menu.MenuOption.TAPAS;
+import static christmas.domain.menu.MenuOption.T_BONE_STEAK;
+import static christmas.domain.menu.MenuOption.ZERO_COKE;
+
+import java.util.List;
+
 public enum MenuGroup {
+
+    APPETIZER("애피타이저", List.of(MUSHROOM_SOUP, TAPAS, CAESAR_SALAD)),
+    MAIN_DISH("메인", List.of(T_BONE_STEAK, BBQ_LIP, SEAFOOD_PASTA, CHRISTMAS_PASTA)),
+    DESSERT("디저트", List.of(CHOCO_CAKE, ICE_CREAM)),
+    BEVERAGE("음료", List.of(ZERO_COKE, RED_WINE, CHAMPAGNE));
+
+
+    private final String name;
+    private final List<MenuOption> menuOptions;
+
+    MenuGroup(String name, List<MenuOption> menuOptions) {
+        this.name = name;
+        this.menuOptions = menuOptions;
+    }
 }
