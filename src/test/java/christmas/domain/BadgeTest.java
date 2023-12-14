ad3784ed8930b2e278edb5715e848ca3f19af921
linirini <2001yerin@naver.com>
2023-12-14 15:57:54 +0900

    test : test for getting star badge

---
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -0,0 +1,18 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class BadgeTest {
+
+    @DisplayName("혜택 금액이 5000원 이상 10000원 미만이면 별 뱃지를 부여한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {5000, 6000, 9000})
+    void 별_뱃지_부여(int amount) {
+        assertThat(Badge.getBadge(amount)).isEqualTo(Badge.STAR);
+    }
+
+}
