30b38ef435e81da7c8c67d0e5345b87c3e1e07d3
linirini <2001yerin@naver.com>
2023-12-14 16:09:12 +0900

    test : test for getting no gift event benefit price

--- src/test/java/christmas/domain/BenefitTest.java
+++ src/test/java/christmas/domain/BenefitTest.java
@@ -19,6 +19,14 @@ class BenefitTest {
         assertThat(createBenefit().getGiftEventBenefitPrice()).isEqualTo(25000);
     }
 
+    @DisplayName("증정품을 안 받았을때는 0원을 반환한다.")
+    @Test
+    void 증정품_미수여_금액() {
+        assertThat(
+                new Benefit(3400, 2023, 0, 1000, Gift.NONE).getGiftEventBenefitPrice()).isEqualTo(
+                0);
+    }
+
     private Benefit createBenefit() {
         return new Benefit(3400, 2023, 0, 1000, Gift.GIFT);
     }
