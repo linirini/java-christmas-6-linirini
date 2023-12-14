0bc9c70c2d79570978be96c8c8305aa1e6bf370d
linirini <2001yerin@naver.com>
2023-12-14 15:59:04 +0900

    test : test for receiving no badge

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -8,6 +8,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class BadgeTest {
 
+    @DisplayName("혜택 금액이 5000원 미만이면 뱃지를 부여받지 않는다.")
+    @ParameterizedTest
+    @ValueSource(ints = {500, 600, 4000})
+    void 뱃지_미부여(int amount) {
+        assertThat(Badge.getBadge(amount)).isEqualTo(Badge.NONE);
+    }
+
     @DisplayName("혜택 금액이 5000원 이상 10000원 미만이면 별 뱃지를 부여한다.")
     @ParameterizedTest
     @ValueSource(ints = {5000, 6000, 9000})
